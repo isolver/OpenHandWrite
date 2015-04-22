71245a83cb7b3be606526511cdafedbef93d56c1
Sol Simpson <sol@isolver-software.com>
2015-04-22 02:02:50 -0400

    ENH: #6

--- freezeApp.py
+++ freezeApp.py
@@ -16,6 +16,7 @@ excludes = ['cvxopt','_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 'pywin.deb
     'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl','tables',
     'Tkconstants', 'Tkinter', 'zmq','PySide','pysideuic','scipy','matplotlib']
 
+includefiles =['markwrite/resources','test_data']
 if sys.version[0] == '2':
     # causes syntax error on py2
     excludes.append('PyQt4.uic.port_v3')
@@ -25,12 +26,14 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 build_exe_options = {
+    'build_exe': 'build/MarkWrite',
     'excludes': excludes,
     'includes':includes,
     'include_msvcr':True,
     'compressed':True,
     'copy_dependent_files':True,
     'create_shared_zip':True,
+    'include_files':includefiles,
     'include_in_shared_zip':True,
     'optimize':2}
 
@@ -38,7 +41,6 @@ setup(name = "MarkWrite",
       version = markwrite.__version__,
       description = "MarkWrite Application",
       options = {"build_exe": build_exe_options},
-      executables = [Executable("./markwrite/runapp.py", base=base)])
-
+      executables = [Executable(script="./markwrite/runapp.py", targetName='MarkWrite.exe',base=base)])
 
 
