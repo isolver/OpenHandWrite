8a42af66da44365f9a0ce6a91dc8da783825230d
Sol Simpson <sol@isolver-software.com>
2016-02-03 14:43:37 -0500

    removed spaces from bat file names
    + updated markwrite setup.py classifiers.

--- src/markwrite/setup.py
+++ src/markwrite/setup.py
@@ -6,13 +6,16 @@ from markwrite import __version__
 
 setup(
       classifiers=[ #http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      'Development Status :: 3 - Alpha',
-      'Environment :: Windows',
+      'Development Status :: 4 - Beta',
+      'Environment :: Win32 (MS Windows)',
       'Intended Audience :: Science/Research',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
       'Operating System :: Microsoft :: Windows',
-      'Programming Language :: Python',
+      'Natural Language :: English'
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: Implementation :: CPython',
       'Topic :: Scientific/Engineering',
+      'Topic :: Software Development :: Libraries :: Python Modules'
       ],
 
     name='markwrite',
@@ -23,7 +26,7 @@ setup(
         'markwrite': ['resources/icons/*.png','resources/tags/*.tag'],
         },
     url='https://github.com/isolver/OpenHandWrite#markwrite',
-    license='GPL',
+    license='GPLv3+',
     author='Sol Simpson',
     author_email='sol@isolver-software.com',
     description='MarkWrite, part of the OpenHandWrite Project, is a tool for the inspection and segmentation of digitized writing data.'
