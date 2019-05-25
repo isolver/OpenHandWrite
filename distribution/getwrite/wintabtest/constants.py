413e90707c5ddd4c1089e05b2361055486e1089a
Sol Simpson <sol@isolver-software.com>
2019-05-25 07:25:49 -0400

    Updated wintab test constants

--- distribution/getwrite/wintabtest/constants.py
+++ distribution/getwrite/wintabtest/constants.py
@@ -15,17 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
-#
-### Misc. constants or general settings that may want to be changed if
-### modifying the example python experiment template.
-#
-
 import os
 
-EXP_NAME =  u'OpenHW Template'
-EXP_VERSION = "0.2"
-
+# Default session data file name
 DEFAULT_SESSION_CODE = u's1234'
 
 # RGB255 color to use for the experiment window background color. Must be a
@@ -46,23 +38,47 @@ ACCEPTED_IMAGE_FORMATS = ['bmp', 'png']
 # keyboard key press event.
 TERMINATE_EXP_KEYS = ['escape',]
 
+# Defaults for PenPositionStim
+# Pen gaussian point color when hover is detected
+PEN_POS_HOVER_COLOR = (0, 0, 255)
+
+# Pen gaussian point color when pen press is detected
+PEN_POS_TOUCHING_COLOR = (0, 255, 0)
+
+# Color of the pen tilt line graphic 
+PEN_POS_ANGLE_COLOR = (255, 255, 0) 
+
+# Pixel width of the pen tilt line graphic 
+PEN_POS_ANGLE_WIDTH = 1
+
+# Control the overall length of the pen tilt line graphic.
+# 1.0 = default length. Set to < 1.0 to make line shorter, or > 1.0 to
+# make line longer. 
+PEN_POS_TILTLINE_SCALAR = 1.0
+
+# Minimum opacity value allowed for pen position graphics.
+# 0.0 = pen position disappears when pen is not detected.
+PEN_POS_GFX_MIN_OPACITY = 0.0
+
+# Minimum pen position graphic size, in normal coord space.
+PEN_POS_GFX_MIN_SIZE = 0.033
+
+# Maximum pen position graphic size, in normal coord space, is equal to
+# PEN_POS_GFX_MIN_SIZE+PEN_POS_GFX_SIZE_RANGE 
+PEN_POS_GFX_SIZE_RANGE = 0.1666
+
+# Defaults for PenTracesStim
+# Width of pen trace line graphics (in pixels)
+PEN_TRACE_LINE_WIDTH = 2
+
+# Pen trace line color (in r,g,b 0-255)
+PEN_TRACE_LINE_COLOR=(0, 0, 0)
+
+# Pen trace line opacity. 0.0 = hidden / fully transparant, 1.0 = fully visible
+PEN_TRACE_LINE_OPACITY=1.0
+
 # Constants for the absolute paths to various experiment subfolders
 AUDIO_FOLDER = os.path.join(os.path.dirname(__file__),u'resources',u'audio')
 IMAGE_FOLDER = os.path.join(os.path.dirname(__file__),u'resources',u'image')
 CONDITIONS_FOLDER =  os.path.join(os.path.dirname(__file__),u'conditions')
-
-# Defaults for PenPositionStim
-PEN_POS_HOVER_COLOR = (0, 0, 255)
-PEN_POS_TOUCHING_COLOR = (0, 255, 0)
-PEN_POS_ANGLE_COLOR = (255, 255, 0) 
-PEN_POS_ANGLE_WIDTH = 1
-PEN_POS_TILTLINE_SCALAR = 1.0
-PEN_POS_GFX_MIN_OPACITY = 0.0
-PEN_POS_GFX_MIN_SIZE = 0.033
-PEN_POS_GFX_SIZE_RANGE = 0.1666
-
-#Defaults for PenTracesStim
-PEN_TRACE_LINE_WIDTH = 2
-PEN_TRACE_LINE_COLOR=(0, 0, 0)
-PEN_TRACE_LINE_OPACITY=1.0
 
